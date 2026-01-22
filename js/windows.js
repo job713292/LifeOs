5e930d8c4a7d3cf40a764445dfb14e9782f5a8b3
Job <jobnunez713@gmail.com>
2026-01-22 11:19:20 -0500

    Prueba de arrastre de ventanas 1

--- js/windows.js
+++ js/windows.js
@@ -1,13 +1,14 @@
-const btn = document.querySelectorAll(".icon");
+const botones = document.querySelectorAll(".icon");
 const ventana = document.getElementById("ventanas");
 const windowdd = document.getElementById("windows-container");
 const desktop = document.getElementById("desktop-icons");
 const cerrarVentana = document.getElementById("cerrarVentana")
 
-btn.forEach(icono => {
+
+//Abrir ventana 
+botones.forEach(icono => {
   icono.addEventListener("click", () => {
-    ventana.classList.remove("oculto");
-    windowdd.style.zIndex = "3"; 
+    abrirVentana();
   });
 });
 
@@ -20,4 +21,40 @@ window.onclick = function(event) {
 
 cerrarVentana.addEventListener("click", function(){
   ventana.classList.add("oculto");
+});
+
+function abrirVentana(){
+  ventana.classList.remove("oculto");
+  windowdd.style.zIndex = "3"; 
+}
+
+
+//prueba de mover ventanas 
+
+const titleBar = document.getElementById("titleBar");
+
+let isDragging = false;
+let offsetX = 0;
+let offsetY = 0;
+
+titleBar.addEventListener("mousedown", (e) =>{
+  isDragging = true;
+
+  const rect = ventana.getBoundingClientRect();
+
+  offsetX = e.clientX - rect.left;
+  offsetY = e.clientY - rect.top;
+  titleBar.style.cursor = "grabbing";
+});
+
+document.addEventListener("mousemove", (e) => {
+  if (!isDragging) return;
+
+  ventana.style.left = e.clientX - offsetX + "px";
+  ventana.style.top = e.clientY - offsetY + "px";
+});
+
+document.addEventListener("mouseup", () => {
+  isDragging = false;
+  titleBar.style.cursor = "";
 });
